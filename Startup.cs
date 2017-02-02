22e0f7d867708f5c67bed90419f5500f45c4a2cb
Angel Murchison <angelmurchison09@gmail.com>
2017-02-02 13:02:27 -0500

    Debugging Auth0 token validation.

--- Startup.cs
+++ Startup.cs
@@ -2,41 +2,37 @@
 using System.Web.Http;
 using EcoHostelAPI;
 using Microsoft.Web;
-using Microsoft.Owin.Cors;
-using Microsoft.Owin.Security.DataHandler.Encoder;
-using Microsoft.Owin.Security.Jwt;
-
 using Owin;
+using System.Configuration;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Microsoft.IdentityModel.Tokens;
 
-using AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode;
-
-[assembly: OwinStartup(typeof(Startup))]
 namespace EcoHostelAPI
 {
     public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            var issuer = WebConfigurationManager.AppSettings["Auth0Domain"];
-            var audience = WebConfigurationManager.AppSettings["Auth0ClientID"];
-            var secret = TextEncodings.Base64Url.Decode(WebConfigurationManager.AppSettings["Auth0ClientSecret"]);
+            var issuer = $"https://{ConfigurationManager.AppSettings["Auth0Domain"]}/";
+            var audience = ConfigurationManager.AppSettings["Auth0ClientID"];
 
-            // Owin configuration.
-            app.UseCors(CorsOptions.AllowAll);
-            app.UseJwtBearerAuthentication(new JwtBearerAuthenticationOptions
-            {
-                AuthenticationMode = AuthenticationMode.Active,
-                AllowedAudiences = new[] { audience },
-                IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+            // Api controllers with an [Authorize] attribute will be validated with JWT
+            app.UseActiveDirectoryFederationServicesBearerAuthentication(
+                new ActiveDirectoryFederationServicesBearerAuthenticationOptions
                 {
-                    new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret)
-                },
-            });
+                    TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidAudience = audience,
+                        ValidIssuer = issuer,
+                        IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) => parameters.IssuerSigningTokens.FirstOrDefault()?.SecurityKeys?.FirstOrDefault()
+                    },
+            // Setting the MetadataEndpoint so the middleware can download the RS256 certificate
+            MetadataEndpoint = $"{issuer.TrimEnd('/')}/wsfed/{audience}/FederationMetadata/2007-06/FederationMetadata.xml"
+                });
 
-            // Setup web api.
-            var configuration = new HttpConfiguration();
-            configuration.MapHttpAttributeRoutes();
-            app.UseWebApi(configuration);
+
+            // Configure Web API
+            WebApiConfig.Configure(app);
         }
     }
 }
