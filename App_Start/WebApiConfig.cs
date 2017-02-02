5da44c5813f78e0aa0c039a59448cc877e128e14
Angel Murchison <angelmurchison09@gmail.com>
2017-02-02 17:03:02 -0500

    Auth0 implemented using System.Web

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using EcoHostelAPI.App_Start;
+using System.Web.Configuration;
 
 namespace EcoHostelAPI
 {
@@ -11,6 +13,15 @@ namespace EcoHostelAPI
         {
             // Web API configuration and services
 
+            var clientID = WebConfigurationManager.AppSettings["auth0:ClientId"];
+            var clientSecret = WebConfigurationManager.AppSettings["auth0:ClientSecret"];
+
+            config.MessageHandlers.Add(new JsonWebTokenValidationHandler()
+            {
+                Audience = clientID,
+                SymmetricKey = clientSecret,
+                IsSecretBase64Encoded = false
+            });
             // Web API routes
             config.MapHttpAttributeRoutes();
 
